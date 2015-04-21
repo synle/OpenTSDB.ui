1d9bd87a1da085580cfa45aeddc05c867ef5e892
Sy Le <lenguyensy@gmail.com>
2015-04-20 21:03:19 -0700

    gulp all the way in build css, js and html

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,26 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+var clean = require('gulp-clean');
 
 
+//clean
+gulp.task('clean', function () {
+  return gulp.src(
+  		[
+  			'public/css/*',
+  			'public/js/*',
+  			'public/view/*'
+  		],
+  		{
+  			read: false
+		}
+  	)
+    .pipe(clean());
+});
 
+
+//build
 gulp.task('js', function() {
     gulp.src('src/js/main.js', {
         read: false
@@ -19,4 +36,15 @@ gulp.task('css', function() {
         ['src/css/lib/*', 'src/css/main.css']
     ).pipe(concat('app.css'))
     .pipe(gulp.dest('./public/css'));
+});
+
+
+
+gulp.task('html', function() {
+    return gulp.src(
+        'src/view/**/*',
+        {
+        	base: 'src/view'
+    	}
+    ).pipe(gulp.dest('./public/view'));
 });
