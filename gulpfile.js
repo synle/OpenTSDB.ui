e4b397ba48efb529efa896dea574221431249d2b
Sy Le <lenguyensy@gmail.com>
2015-04-20 21:07:30 -0700

    gulp watch and reload for faster development

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,21 @@ var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var clean = require('gulp-clean');
+var livereload = require('gulp-livereload');
 
 
 //clean
-gulp.task('clean', function () {
-  return gulp.src(
-  		[
-  			'public/css/*',
-  			'public/js/*',
-  			'public/view/*'
-  		],
-  		{
-  			read: false
-		}
-  	)
-    .pipe(clean());
+gulp.task('clean', function() {
+    return gulp.src(
+            [
+                'public/css/*',
+                'public/js/*',
+                'public/view/*'
+            ], {
+                read: false
+            }
+        )
+        .pipe(clean());
 });
 
 
@@ -26,25 +26,32 @@ gulp.task('js', function() {
     gulp.src('src/js/main.js', {
         read: false
     }).pipe(browserify())
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('./public/js'))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('./public/js'))
 });
 
 
 gulp.task('css', function() {
     return gulp.src(
-        ['src/css/lib/*', 'src/css/main.css']
-    ).pipe(concat('app.css'))
-    .pipe(gulp.dest('./public/css'));
+            ['src/css/lib/*', 'src/css/main.css']
+        ).pipe(concat('app.css'))
+        .pipe(gulp.dest('./public/css'));
 });
 
 
 
 gulp.task('html', function() {
     return gulp.src(
-        'src/view/**/*',
-        {
-        	base: 'src/view'
-    	}
+        'src/view/**/*', {
+            base: 'src/view'
+        }
     ).pipe(gulp.dest('./public/view'));
+});
+
+//dev: watch and live reload
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('src/css/**/*', ['css']);
+    gulp.watch('src/js/**/*', ['js']);
+    gulp.watch('src/view/**/*', ['html']);
 });
